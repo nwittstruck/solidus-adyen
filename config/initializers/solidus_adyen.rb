c886f6475e74ad66331662337aefae451e836cb7
nwittstruck <nwittstruck@users.noreply.github.com>
2015-11-23 11:28:32 +0100

    set environment based on test_mode flag, not based on server environment

--- config/initializers/solidus_adyen.rb
+++ config/initializers/solidus_adyen.rb
@@ -1,1 +1,7 @@
 Rails.application.config.assets.precompile += %w( spree/checkout/payment/adyen.js )
+
+# set the test mode on the adyen gem:
+test_mode = Spree::Gateway::AdyenHPP.first.try { | adyenHPP | adyenHPP.preferences[:test_mode] }
+
+# default should be :test (e.g. test_mode == true || test_mode.nil?)
+Rails.application.config.adyen.environment = test_mode == false ? :live : :test
